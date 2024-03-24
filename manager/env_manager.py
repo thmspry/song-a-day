76af94defb614e7c6e70caf75cd53678dbe5bfda
Thomas Peray <thomas.peray5@gmail.com>
2024-03-24 14:29:44 +0100

    Rewrite endpoint for env variables

--- manager/env_manager.py
+++ manager/env_manager.py
@@ -1,42 +1,24 @@
-import json
 import os
-from dotenv import load_dotenv, find_dotenv
+from dotenv import dotenv_values
 
-import utils
+import manager.file_manager as fm
 
-def load_env() -> dict:
-    env_expected_variables = __load_env_expected_variables__()
-    def list_variables(variables_to_list: list[str], variable_descriptions: dict) -> str:
-        r_str = ""
-        for v_name in variables_to_list:
-            r_str += f"\t- {v_name} : {variable_descriptions[v_name]}"
-        return r_str
-            
-    if not utils.__file_exists__(".env"):
-        msg_err = "Le fichier .env n'existe pas. Veuillez le créer en renseignant les variables suivantes :"
-        raise Exception(msg_err + list_variables(env_expected_variables.keys(), env_expected_variables))
+DOTENV_PATH = ".env"
+expected_variable_names = ["SPREADSHEET_ID", "API_KEY", "SESSION_ID_TIKTOK"]
 
-    load_dotenv(find_dotenv())
-    missing_variables = []
-    env = {}
-    for v_name in env_expected_variables:
-        VALUE = os.environ.get(v_name)
-        if VALUE is None:
-            missing_variables.append(v_name)
-        else:
-            env[v_name] = VALUE
-      
-    if len(missing_variables) > 0:
-        one_or_two = "des variables" if len(missing_variables) > 1 else "une variable"
-        msg_err = f"Il manque {one_or_two} dans le fichier .env :"
-        raise Exception(msg_err + list_variables(missing_variables, env_expected_variables))
+def load_env() -> list[dict]:    
+    if not fm.file_exists(DOTENV_PATH):
+        raise Exception("Le fichier .env n'existe pas.")
+    
+    config = dotenv_values(DOTENV_PATH)
+    
+    env: list[dict] = []
+    
+    for v_name in expected_variable_names:
+        value = None
+        if v_name in config:
+            value = config[v_name]
+        env_variable = {"name": v_name, "value": value}
+        env.append(env_variable)
         
     return env
-
-def __load_env_expected_variables__(file_path: str = "env_description.json") -> dict:
-    with open(file_path, 'r') as file_json:
-        # Charger le JSON
-        json_data = file_json.read()
-        data = json.loads(json_data)
-        # Transformer en un dictionnaire
-        return {item["key"]: item["description"] for item in data}
